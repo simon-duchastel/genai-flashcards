0794f067db017be9ae5e6281aab8e50d3995c805
Simon Duchastel <simon@duchastel.com>
2025-10-21 00:18:02 -0400

    missed a spot where hotreload was used

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.kotlinSerialization) apply false
 }
